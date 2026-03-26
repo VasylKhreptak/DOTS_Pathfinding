51ee870c630719d235869a49b423201c0f54965d
VasylKhreptak <215703117+VasylKhreptakMidnight@users.noreply.github.com>
2026-03-26 21:20:40 +0200

    Update PathDrawSystem.cs

--- Assets/Scripts/Entities/Systems/Pathfinding/PathDrawSystem.cs
+++ Assets/Scripts/Entities/Systems/Pathfinding/PathDrawSystem.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 namespace Entities.Systems.Pathdinding.Editor
 {
     [UpdateInGroup(typeof(PresentationSystemGroup))]
+    [DisableAutoCreation]
     public partial class PathDrawSystem : SystemBase
     {
         protected override void OnUpdate()
