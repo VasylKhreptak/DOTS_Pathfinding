d41a1b377e37755704f371665e217b2a7a6a8613
VasylKhreptak <215703117+VasylKhreptakMidnight@users.noreply.github.com>
2026-03-27 21:37:13 +0200

    Update NavMeshBakeSystem.cs

--- Assets/Scripts/Entities/Systems/Pathfinding/NavMeshBakeSystem.cs
+++ Assets/Scripts/Entities/Systems/Pathfinding/NavMeshBakeSystem.cs
@@ -34,7 +34,7 @@ namespace Entities.Systems.Pathfinding
     public partial class NavMeshBakeSystem : SystemBase
     {
         private const float BakeInterval = 2f;
-        private const float Range = 500f;
+        private const float Range = 100f;
         private const int InitialSourcesBufferSize = 1024 * 2;
         private const int NavMeshSourceConversionBatchCount = 64;
 
