056624cefba4eae5ed424d0da5e52433d9cd8b4c
VasylKhreptak <215703117+VasylKhreptakMidnight@users.noreply.github.com>
2026-04-03 22:08:44 +0300

    feat: added base agent authoring editor;

---
+++ Assets/Scripts/Editor/Pathfinding/AgentAuthoringEditor.cs
@@ -0,0 +1,64 @@
+﻿using Entities.Authoring.Pathfinding;
+using UnityEditor;
+using UnityEngine.AI;
+
+namespace Editor.Pathfinding
+{
+    [CustomEditor(typeof(AgentAuthoring))]
+    [CanEditMultipleObjects]
+    public class AgentAuthoringEditor : UnityEditor.Editor
+    {
+        private SerializedProperty _agentTypeProp;
+
+        private void OnEnable()
+        {
+            _agentTypeProp = serializedObject.FindProperty("_agentType");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawAgentTypeField();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawAgentTypeField()
+        {
+            int count = NavMesh.GetSettingsCount();
+            string[] agentTypeNames = new string[count];
+            int[] agentTypeIDs = new int[count];
+            int selectedIndex = -1;
+
+            for (int i = 0; i < count; i++)
+            {
+                NavMeshBuildSettings settings = NavMesh.GetSettingsByIndex(i);
+                agentTypeIDs[i] = settings.agentTypeID;
+                agentTypeNames[i] = NavMesh.GetSettingsNameFromID(settings.agentTypeID);
+
+                if (agentTypeIDs[i] == _agentTypeProp.intValue)
+                {
+                    selectedIndex = i;
+                }
+            }
+
+            EditorGUI.BeginChangeCheck();
+
+            if (selectedIndex == -1 && count > 0)
+                selectedIndex = 0;
+
+            int newIndex = EditorGUILayout.Popup("Agent Type", selectedIndex, agentTypeNames);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                if (newIndex >= 0 && newIndex < count)
+                {
+                    _agentTypeProp.intValue = agentTypeIDs[newIndex];
+                }
+            }
+
+            DrawPropertiesExcluding(serializedObject, "_agentType", "m_Script");
+        }
+    }
+}
--- Assets/Scripts/Entities/Authoring/Pathfinding/AgentAuthoring.cs
+++ Assets/Scripts/Entities/Authoring/Pathfinding/AgentAuthoring.cs
@@ -8,13 +8,18 @@ namespace Entities.Authoring.Pathfinding
     [RequireComponent(typeof(SeekerAuthoring))]
     public class AgentAuthoring : MonoBehaviour
     {
+        [SerializeField] private int _agentType;
+
         private class AgentBaker : Baker<AgentAuthoring>
         {
             public override void Bake(AgentAuthoring authoring)
             {
                 Entity entity = GetEntity(TransformUsageFlags.Dynamic);
 
-                Agent agent = new Agent();
+                Agent agent = new Agent()
+                {
+                    AgentID = authoring._agentType
+                };
 
                 AddComponent(entity, agent);
             }
